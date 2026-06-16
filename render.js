1b2faa0c8c467a092a65a22e08296806463b0274
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-06-16 08:15:25 -0400

    Create render.js

---
+++ render.js
@@ -0,0 +1,42 @@
+import { renderInicio } from "./pages/inicio.js";
+import { renderNuestroEquipo } from "./pages/nuestro_equipo.js";
+import { renderServicios } from "./pages/servicios.js";
+import { renderConvenios } from "./pages/convenios.js";
+import { renderBlog } from "./pages/blog.js";
+import { renderContacto } from "./pages/contacto.js";
+import { renderGaleria } from "./pages/galeria.js";
+
+const content = document.getElementById("content");
+
+function router() {
+    const route = location.hash || "#inicio";
+
+    if (route === "#inicio") {
+        renderInicio(content);
+    }
+  
+    } else if (route === "#nuestro-equipo") {
+        renderNuestroEquipo(content);
+
+    } else if (route === "#servicios") {
+        renderServicios(content);
+
+    } else if (route === "#convenios") {
+        renderConvenios(content);
+
+   } else if (route === "#blog") {
+        renderBlog(content);
+
+   } else if (route === "#contacto") {
+        renderContacto(content);
+
+   } else if (route === "#galeria") {
+        renderGaleria(content);
+
+   } else {
+        renderInicio(content);
+   }
+}
+
+window.addEventListener("hashchange", router);
+window.addEventListener("load", router);
